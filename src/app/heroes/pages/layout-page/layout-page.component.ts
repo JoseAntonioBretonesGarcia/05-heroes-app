8339e972b32581a8e575fcd54a9a80244391cb68
JoseAntonioBretonesGarcia <joseantoniobregar@gmail.com>
2023-06-05 17:21:11 +0200

    Section 14, class 193.

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public sideBarItems = [
+    { label: 'Heroes', icon: 'label', url: './list' },
+    { label: 'Add Hero', icon: 'add', url: './new-hero' },
+    { label: 'Search', icon: 'search', url: './search' },
+  ];
 }
